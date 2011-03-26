308b819b424ca2ede9b7f0fd3d545120db22c897
c-spencer <cs560@cam.ac.uk>
2011-03-26 22:34:57 +0000

    Add sanitiser, modified from andris9/node-markdown

--- src/showdown.js
+++ src/showdown.js
@@ -1,6 +1,10 @@
 //
 // showdown.js -- A javascript port of Markdown.
 //
+// Copyright (c) 2011 Chris Spencer
+// 
+// Copyright (c) 2010 Andris Reinman
+//
 // Copyright (c) 2007 John Fraser.
 //
 // Original Markdown Copyright (c) 2004-2005 John Gruber
@@ -88,13 +92,17 @@ var g_html_blocks;
 var g_list_level = 0;
 
 
-this.makeHtml = function(text) {
+this.makeHtml = function(text, strip) {
 //
 // Main function. The order in which other subs are called here is
 // essential. Link and image substitutions need to happen before
 // _EscapeSpecialCharsWithinTagAttributes(), so that any *'s or _'s in the <a>
 // and <img> tags get encoded.
 //
+
+  if (strip === undefined) {
+    strip = true;
+  }
 
   // Clear the global hashes. If we don't clear these, you get conflicts
   // from other articles when generating a page which contains more than
@@ -146,6 +154,10 @@ this.makeHtml = function(text) {
 
   // attacklab: Restore tildes
   text = text.replace(/~T/g,"~");
+  
+  if (strip) {
+    text = stripUnwantedHTML(text);
+  }
 
   return text;
 }
@@ -1315,6 +1327,101 @@ var escapeCharacters_callback = function(wholeMatch,m1) {
 }
 
 } // end of Showdown.converter
+
+// Sourced from https://github.com/andris9/node-markdown
+var stripUnwantedHTML = function (html /*, allowedTags, allowedAttributes, forceProtocol */){
+    var allowedTags = arguments[1] || 
+            'a|b|blockquote|code|del|dd|dl|dt|em|h1|h2|h3|h4|h5|h6|'+
+            'i|img|li|ol|p|pre|sup|sub|strong|strike|ul|br|hr|span|'+
+            'table|th|tr|td|tbody|thead|tfoot',
+        allowedAttributes = arguments[2] || {
+            'img': 'src|width|height|alt',
+            'a':   'href',
+            '*':   'title',
+            'span': 'class',
+            'tr': 'rowspan',
+            'td': 'colspan|align',
+            'th': 'rowspan|align'
+        }, forceProtocol = arguments[3] || true;
+        
+        testAllowed = new RegExp('^('+allowedTags.toLowerCase()+')$'),
+        findTags = /<(\/?)\s*([\w:\-]+)([^>]*)>/g,
+        findAttribs = /(\s*)([\w:-]+)\s*=\s*(?:(?:(["'])([^\3]+?)(?:\3))|([^\s]+))/g;
+    
+    // convert all strings patterns into regexp objects (if not already converted)
+    for(var i in allowedAttributes){
+        if(allowedAttributes.hasOwnProperty(i) && typeof allowedAttributes[i] === 'string'){
+            allowedAttributes[i] = new RegExp('^('+
+                allowedAttributes[i].toLowerCase()+')$');
+        }
+    }
+    
+    // find and match html tags
+    return html.replace(findTags, function(original, lslash, tag, params){
+        var tagAttr, wildcardAttr, 
+            rslash = params.substr(-1)=="/" && "/" || "";
+
+        tag = tag.toLowerCase();
+        
+        // tag is not allowed, return empty string
+        if(!tag.match(testAllowed))
+            return "";
+        
+        // tag is allowed
+        else{
+            // regexp objects for a particular tag
+            tagAttr = tag in allowedAttributes && allowedAttributes[tag];
+            wildcardAttr = "*" in allowedAttributes && allowedAttributes["*"];
+            
+            // if no attribs are allowed
+            if(!tagAttr && !wildcardAttr)
+                return "<"+lslash+tag+rslash+">";
+            
+            // remove trailing slash if any
+            params = params.trim();
+            if(rslash){
+                params = params.substr(0, params.length-1);
+            }
+            
+            // find and remove unwanted attributes
+            params = params.replace(findAttribs, function(original, space,
+                                                            name, quot, value){
+                name = name.toLowerCase();
+                
+                if (!value && !quot) {
+                  value = "";
+                  quot = '"';
+                } else if (!value) {
+                  value = quot;
+                  quot = '"';
+                }
+                
+                // force data: and javascript: links and images to #
+                if((name=="href" || name=="src") &&
+                   (value.trim().substr(0, "javascript:".length)=="javascript:"
+                    || value.trim().substr(0, "data:".length)=="data:")) {
+                    value = "#";
+                }
+                
+                // scope links and sources to http protocol
+                if (forceProtocol &&
+                     (name=="href" || name=="src") &&
+                     !/^[a-zA-Z]{3,5}:\/\//.test(value)) {
+                  value = "http://" + value;
+                }
+                
+                if((wildcardAttr && name.match(wildcardAttr)) ||
+                        (tagAttr && name.match(tagAttr))){
+                    return space+name+"="+quot+value+quot;
+                }else
+                    return "";
+            });
+
+            return "<"+lslash+tag+(params?" "+params:"")+rslash+">";
+        }
+            
+    });
+}
 
 // export
 if (typeof exports != 'undefined') exports.Showdown = Showdown;
